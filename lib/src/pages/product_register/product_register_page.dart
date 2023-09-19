a11c2daf3bb937d138a27c2e777fcd41a10e8b31
Gilvan Costa <gilvancosta2025@gmail.com>
2023-09-19 08:25:47 -0300

    desenvolvendo o cadastro de produtos

--- lib/src/pages/product_register/product_register_page.dart
+++ lib/src/pages/product_register/product_register_page.dart
@@ -9,13 +9,37 @@ class ProductRegisterPage extends StatefulWidget {
 }
 
 class _ProductRegisterPageState extends State<ProductRegisterPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cadastro de Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+          ),],
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+              const Row(
+                children: [
+
+                ],
+                
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
